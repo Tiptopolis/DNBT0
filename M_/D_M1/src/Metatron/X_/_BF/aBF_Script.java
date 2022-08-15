35ff7e77898c622702f73a34397e7cd925d6bcba
Daniel G <dgordon19990@gmail.com>
2022-08-15 17:00:11 -0500

    1

--- M_/D_M1/src/Metatron/X_/_BF/aBF_Script.java
+++ M_/D_M1/src/Metatron/X_/_BF/aBF_Script.java
@@ -6,49 +6,145 @@ import Metatron.Core.Primitive.Struct.aMap;
 import Metatron.Core.Primitive.Struct.aQueue;
 import Metatron.Core.Utils.iCypher;
 
-public class aBF_Script {
-	
-	
+public class aBF_Script implements iFunctor.Function<aBF_Script,_Array<Integer>>{
+
 	protected String alpabet;
+	String script;
 
-	aQueue<String> op;
-	_Array<Integer> out;// 32-Bit cells
+	aQueue<String> op;	
+	_Array<Integer> data;// 32-Bit cells
 
-	long data;
-	long instruction; // index
+	int dataPointer;
+	int cellValue; // index
 
 	public int cellMod = 16;
 	protected boolean wrap = true;
-	
-	public static aMap<String,iFunctor> Commands;
-	public static aMap<String, iFunctor> CommandMap;
-	
+
+	public static aMap<String, iFunctor> CommandNames;
+	public static aMap<String, iFunctor> CommandSymbols;
+
 	static {
-		Commands = new aMap<String,iFunctor>();
-		CommandMap = new aMap<String,iFunctor>();
-		
-		iFunctor.Effect<aBF_Script> incrementD = (a)-> {a.data++; return a;};
-		iFunctor.Effect<aBF_Script> decrementD = (a)-> {a.data--; return a;};
-		Commands.put("IncrementData",incrementD);
-		Commands.put("DecrementData",decrementD);
+		CommandNames = new aMap<String, iFunctor>();
+		CommandSymbols = new aMap<String, iFunctor>();
+
+		iFunctor.Effect<aBF_Script> incrementD = (a) -> {
+			a.dataPointer++;
+			a.doWrap();
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> decrementD = (a) -> {
+			a.dataPointer--;
+			a.doWrap();
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> incrementP = (a) -> {
+			a.cellValue++;
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> decrementP = (a) -> {
+			a.cellValue--;
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> put = (a) -> {
+			a.data.set(a.dataPointer, a.cellValue);
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> get = (a) -> {
+			a.cellValue = a.data.get(a.dataPointer);
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> loop = (a) -> {
+
+			if (a.dataPointer == 0)
+				for (int i = a.dataPointer; i < a.data.size(); i++)
+					if (a.script.charAt(i) == ']') {
+						a.dataPointer = i + 1;
+						a.doWrap();
+						return a;
+					}
+
+			return a;
+		};
+
+		iFunctor.Effect<aBF_Script> back = (a) -> {
+
+			if (a.dataPointer == 0)
+				for (int i = 0; i < a.dataPointer; i++)
+					if (a.script.charAt(i) == '[') {
+						a.dataPointer = i + 1;
+						a.doWrap();
+						return a;
+					}
+
+			return a;
+		};
+
+		CommandNames.put("IncrementPointer", incrementP);
+		CommandSymbols.put(">", incrementP);
+		CommandNames.put("DecrementPointer", decrementP);
+		CommandSymbols.put("<", decrementP);
+
+		CommandNames.put("IncrementCell", incrementD);
+		CommandSymbols.put("+", incrementD);
+		CommandNames.put("DecrementCell", decrementD);
+		CommandSymbols.put("-", decrementD);
+
+		CommandNames.put("Put", put);
+		CommandSymbols.put(".", put);
+		CommandNames.put("Get", get);
+		CommandSymbols.put(",", get);
+
+		CommandNames.put("Loop", loop);
+		CommandSymbols.put("[", loop);
+		CommandNames.put("]", back);
+		CommandSymbols.put("Back", back);
+
+		// <> iP
+		// +- iD
+		// . read
+		// , write
+		// [ jump
+		// ] back
 	}
 
 	public aBF_Script() {
-		this(iCypher._REX,16000);
+		this(iCypher._REX, 16000);
 	}
 
 	public aBF_Script(String alphabet, long tapeLen) {
 		this.alpabet = alphabet;
-		out = new _Array<Integer>();
+		data = new _Array<Integer>();
 		for (int i = 0; i < tapeLen; i++)
-			out.append(0);
+			data.append(0);
 	}
 
 	public String parse() {
 		String s = "";
-		for (Integer i : out)
-			s += iCypher.decypher(this.alpabet, out.get(i).intValue());
+		for (Integer i : data)
+			s += iCypher.decypher(this.alpabet, data.get(i).intValue());
 		return s;
 	}
 
+	protected void doWrap() {
+		if (this.wrap)
+			this.dataPointer = this.dataPointer % this.data.size();
+	}
+
+	
+
+
+	@Override
+	public _Array<Integer> apply(Object... t) {
+    //String or Char[]
+
+		
+		return this.data;	
+	}
+	
 }
