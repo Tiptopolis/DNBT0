67c62ae06453e70807507e5403c2c2e6cc40e429
Daniel G <dgordon19990@gmail.com>
2022-08-18 09:57:15 -0500

    1

---
+++ M_/D_M1/src/Metatron/Core/System/Script/_SQL/SQL_Adapter.java
@@ -0,0 +1,14 @@
+package Metatron.Core.System.Script._SQL;
+
+import java.sql.Connection;
+
+public class SQL_Adapter {
+
+	public static final String module = "jdbc";
+	
+	public SQL_Adapter(Connection c)
+	{
+		
+	}
+	
+}
